3861b51bc064e22ce4c696e348ba9e36708422fc
carolmitsuko <carolmitsuko@gmail.com>
2015-01-23 10:09:25 -0200

    alteracao autenticacao

--- settings.py
+++ settings.py
@@ -227,7 +227,7 @@ HELIOS_PRIVATE_DEFAULT = False
 
 # authentication systems enabled
 AUTH_ENABLED_AUTH_SYSTEMS = ['password','facebook','twitter', 'google', 'yahoo']
-AUTH_ENABLED_AUTH_SYSTEMS = get_from_env('AUTH_ENABLED_AUTH_SYSTEMS', 'ldap').split(",")
+#AUTH_ENABLED_AUTH_SYSTEMS = get_from_env('AUTH_ENABLED_AUTH_SYSTEMS', 'ldap').split(",")
 AUTH_DEFAULT_AUTH_SYSTEM = get_from_env('AUTH_DEFAULT_AUTH_SYSTEM', None)
 
 # facebook
